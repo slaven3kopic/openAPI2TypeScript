e59948e961d306442fed9e120b1491e3d8644c35
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2021-01-15 13:11:18 +0100

    new gen lib

---
+++ route.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface IRoute {
+  /** Route href */
+  href: string;
+
+  /** Route label */
+  label: string;
+}
+
+export type IExtendedRoute = IRoute & { timestamp: number };
